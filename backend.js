77b2985ff45a7a616e5436f45fd2191763e090a8
Stephenwb1 <156565077+Stephenwb1@users.noreply.github.com>
2024-12-16 22:19:05 -0800

    made a bit more progress

--- backend.js
+++ backend.js
@@ -5,7 +5,11 @@ const canvas = document.getElementById("canvas")
 const submitButton = document.getElementById('submitButton');
 const ctx = canvas.getContext("2d");
 const preview = document.getElementById("preview");
-const result = document.getElementById("result")
+const result = document.getElementById("result");
+
+// The number of columns we'll use in our ascii image
+const cols = 100;
+const gscale = " .:-=+*#%@"; //dark to light
 
 
 
@@ -32,7 +36,7 @@ imageInput.addEventListener("change", () => {
             preview.src = img.src;
             preview.style.display = "block";
 
-            ctx.drawImage(img, 0, 0)
+            ctx.drawImage(img, 0, 0);
         };
 
         /*preview.src = event.target.result; //sets the image source to the file content
@@ -41,15 +45,26 @@ imageInput.addEventListener("change", () => {
 
     reader.readAsDataURL(file);
 
-    
-
 });
 
 submitButton.addEventListener("click", () => {
+    
+    const resultImage = createGrayscale();
+
+    ctx.putImageData(resultImage, 0, 0);
+
+    result.src = canvas.toDataURL();
+    result.style.display = "block";
+});
+
+
+
+function createGrayscale() {
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data;
 
     //loop through every pixel (R, G, B, A)
+    //convert to grayscale
     for(let i = 0; i < data.length; i += 4) {
         const grayscale = 0.3 * data[i] + 0.59 * data[i + 1] + 0.11 * data[i + 2];
 
@@ -57,23 +72,24 @@ submitButton.addEventListener("click", () => {
         data[i] = data[i + 1] = data[i + 2] = grayscale;
     }
 
-    ctx.putImageData(imageData, 0, 0);
-
-    result.src = canvas.toDataURL();
-    result.style.display = "block";
-});
-
-//unused as of now
-function createAscii(image) {
-    //step one, convert to grayscale
-    const reader = new FileReader();
-
-    reader.onload = function(event) {
-        result.src = event.target.result; //sets the image source to the file content
-        result.style.display = 'block'; //make the image visible
-    };
+    
+    //we need to split the image into M x N tiles (row x column)
 
     
 
-    reader.readAsDataURL(image);
+
+
+
+    return imageData
 }
+
+//stuck here come back eventually
+const ramp = gscale;
+const rampLength = ramp.length;
+
+const getCharForGrayscale = grayscale =>
+    ramp[Math.ceil(((rampLength - 1) * grayscale) / 255)];
+
+const asciiImage = document.querySelector("pre#ascii");
+
+const drawAscii = grayscale
