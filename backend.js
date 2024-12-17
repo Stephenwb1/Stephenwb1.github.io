94a6fd2da50b5ae71dc62ab804485a1238323454
Stephenwb1 <156565077+Stephenwb1@users.noreply.github.com>
2024-12-17 01:09:30 -0800

    Barely got it working

--- backend.js
+++ backend.js
@@ -6,10 +6,26 @@ const submitButton = document.getElementById('submitButton');
 const ctx = canvas.getContext("2d");
 const preview = document.getElementById("preview");
 const result = document.getElementById("result");
+const asciiImage = document.getElementById("ascii");
 
 // The number of columns we'll use in our ascii image
-const cols = 100;
-const gscale = " .:-=+*#%@"; //dark to light
+const ramp = "@%#*+=-:. ";
+const rampLength = ramp.length;
+
+const getCharForGrayscale = grayscale =>
+    ramp[Math.ceil(((rampLength - 1) * grayscale) / 255)];
+
+const drawAscii = (grayScales, width) => {
+    const ascii = grayScales.reduce((asciiImage, grayScale, index) => {
+        let nextChars = getCharForGrayscale(grayScale);
+        if ((index+1) % width === 0) {
+            nextChars += "\n";
+        }
+
+        return asciiImage + nextChars;
+    }, "");
+    asciiImage.textContent = ascii;
+};
 
 
 
@@ -47,49 +63,90 @@ imageInput.addEventListener("change", () => {
 
 });
 
+const getFontRatio = () => {
+    const pre = document.createElement("pre");
+    pre.style.display = "inline";
+    pre.textContent = " ";
+
+    document.body.appendChild(pre);
+    const {width, height} = pre.getBoundingClientRect();
+    document.body.removeChild(pre);
+
+    return height / width;
+}
+
+const MAXIMUM_WIDTH = 80;
+const MAXIMUM_HEIGHT = 80;
+
+const clampDimensions = (width, height) => {
+    const recitifiedWidth = Math.floor(getFontRatio() * width);
+
+    if (height > MAXIMUM_HEIGHT) {
+        const reducedWidth = Math.floor(recitifiedWidth * MAXIMUM_HEIGHT / height);
+        return [reducedWidth, MAXIMUM_HEIGHT];
+    }
+
+    if (width > MAXIMUM_WIDTH) {
+        const reducedHeight = Math.floor(height * MAXIMUM_WIDTH / recitifiedWidth);
+        return [MAXIMUM_WIDTH, reducedHeight];
+    }
+
+    return [recitifiedWidth, height];
+}
+
 submitButton.addEventListener("click", () => {
+    const file = imageInput.files[0];
     
-    const resultImage = createGrayscale();
+    const reader = new FileReader();
+    reader.onload = (event) => {
+        const image = new Image();
+        image.onload = () => {
+            const [width, height] = clampDimensions(image.width, image.height);
 
-    ctx.putImageData(resultImage, 0, 0);
+            canvas.width = width;
+            canvas.height = height;
+            
+            ctx.drawImage(image, 0, 0, width, height);
+            const grayScales = createGrayscale(ctx, width, height)
 
-    result.src = canvas.toDataURL();
-    result.style.display = "block";
+            preview.style.display = "none";
+
+            //imageInput.style.display = "none";
+            drawAscii(grayScales, width)
+        }
+
+        image.src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+
 });
 
 
 
-function createGrayscale() {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+const createGrayscale = (ctx, width, height) => {
+    const imageData = ctx.getImageData(0, 0, width, height);
     const data = imageData.data;
 
     //loop through every pixel (R, G, B, A)
     //convert to grayscale
-    for(let i = 0; i < data.length; i += 4) {
-        const grayscale = 0.3 * data[i] + 0.59 * data[i + 1] + 0.11 * data[i + 2];
+    const grayScales = [];
 
-        //set RGB to grayscale value
-        data[i] = data[i + 1] = data[i + 2] = grayscale;
+    for(let i = 0; i < data.length; i += 4) {
+        const r = imageData.data[i];
+        const g = imageData.data[i+1];
+        const b = imageData.data[i+2];
+
+        const grayScale = (0.21*r + 0.72*g + 0.07*b);
+
+        grayScales.push(grayScale);
     }
 
     
     //we need to split the image into M x N tiles (row x column)
 
-    
+    ctx.putImageData(imageData, 0, 0);
 
+    return grayScales;
+};
 
-
-
-    return imageData
-}
-
-//stuck here come back eventually
-const ramp = gscale;
-const rampLength = ramp.length;
-
-const getCharForGrayscale = grayscale =>
-    ramp[Math.ceil(((rampLength - 1) * grayscale) / 255)];
-
-const asciiImage = document.querySelector("pre#ascii");
-
-const drawAscii = grayscale
